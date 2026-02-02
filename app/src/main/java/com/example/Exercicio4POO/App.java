b02b4a9728707a2262b9120532c96e77a9eae173
Maiara Ávila <maiaroviski@gmail.com>
2026-02-02 10:43:27 -0300

    Incluindo projetos

---
+++ app/src/main/java/com/example/Exercicio4POO/App.java
@@ -0,0 +1,14 @@
+
+package com.example.Exercicio4POO;
+
+public class App {
+    
+
+    public static void main(String[] args) {
+        
+        Invoice inv = new Invoice(1, "MousePad", 10, 50.0f);
+        
+        System.out.println("Valor total: " + inv.getInvoiceAmount());
+        
+    }
+}
